3fd8f256080c382884c6f1707821d97d95df23ad
zhangmr <zhangmr@zendaimoney.com>
2017-04-10 18:03:15 +0800

    models更改

--- dbs/models.js
+++ dbs/models.js
@@ -5,5 +5,10 @@ module.exports={
         name:{type:String, required:true },
         password:{ type:String, required:true },
         gender:{ type:Boolean, default:true }
+    },
+    commodity:{
+        name:String,
+        price:Number,
+        imgSrc:String
     }
 };
